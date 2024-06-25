f4b7bd75d352c67ffdcd19bff5f19d1a8eb39475
SWorster <131273379+SWorster@users.noreply.github.com>
2024-06-25 14:00:59 -0400

    listener improvements

--- comms/rpi_listener.py
+++ comms/rpi_listener.py
@@ -3,6 +3,8 @@ import configs
 import rpi_to_sensor
 import time
 import os
+import argparse
+
 
 # config settings
 rpi_addr = configs.rpi_addr
@@ -13,45 +15,66 @@ TTL = configs.TTL  # minutes to wait before quitting
 TRIES = configs.TRIES  # number of attempts to make
 
 
+def listen() -> None:
+    echo("Activating listener...")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((rpi_addr, so_port))  # bind the socket to a specific address and port
+    s.listen(5)  # listen for incoming connections
+    echo(f"Listening on {rpi_addr}:{so_port}")
+
+    c, c_address = s.accept()  # accept incoming connections
+    echo(f"Accepted connection from {c_address[0]}:{c_address[1]}")
+
+    # break while after X attempts or X minutes
+    timeout = time.time() + 60 * TTL
+    test = TRIES
+
+    while True:
+        if test == 0 or time.time() > timeout:
+            break
+        test = test - 1
+
+        request_bytes = c.recv(so_msg_size)
+        request = request_bytes.decode(so_encoding)
+
+        if request.lower() == "close":
+            c.send("closed".encode(so_encoding))
+            break
+
+        echo(f"Received: {request}")
+        msg = rpi_to_sensor.to_sensor(request)
+        if msg != "":
+            echo(f"Sending response {msg}")
+            response = msg.encode(so_encoding)  # convert string to bytes
+            c.send(response)
+            c.send("closed".encode(so_encoding))
+            break
+    c.close()
+    s.close()
+    echo("Connection to host closed")
+
+
 def echo(s: str):
     """fancy print statement"""
     os.system(f"echo '{s}'")
 
 
-echo("Activating listener...")
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((rpi_addr, so_port))  # bind the socket to a specific address and port
-server.listen(5)  # listen for incoming connections
-echo(f"Listening on {rpi_addr}:{so_port}")
+def main() -> None:
+    """parses arguments"""
+    parser = argparse.ArgumentParser(
+        prog="rpi_listener.py",
+        description="Handles RPi comms with host computer, using socket",
+        epilog=f"Can give port number as argument, or defaults to ",
+    )
+    parser.add_argument(
+        "port",
+        nargs="?",
+        type=int,
+        help="Provide a port number, or leave empty to use port number from configs.py",
+    )
 
-client, client_address = server.accept()  # accept incoming connections
-echo(f"Accepted connection from {client_address[0]}:{client_address[1]}")
-
-# break while after X attempts or X minutes
-timeout = time.time() + 60 * TTL
-test = TRIES
-
-while True:
-    if test == 0 or time.time() > timeout:
-        break
-    test = test - 1
-
-    request_bytes = client.recv(so_msg_size)
-    request = request_bytes.decode(so_encoding)
-
-    if request.lower() == "close":
-        client.send("closed".encode(so_encoding))
-        break
-
-    echo(f"Received: {request}")
-    msg = rpi_to_sensor.to_sensor(request)
-    if msg != "":
-        echo(f"Sending response {msg}")
-        response = msg.encode(so_encoding)  # convert string to bytes
-        client.send(response)
-        client.send("closed".encode(so_encoding))
-        break
-
-client.close()
-server.close()
-echo("Connection to host closed")
+    args = vars(parser.parse_args())  # get arguments from command line
+    port = args.get("port")
+    if isinstance(port, int):
+        global so_port
+        so_port = port
