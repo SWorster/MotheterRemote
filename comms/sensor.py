8bcdc1d6cf9be7ee3ab3d727b7e8e3629c8a4226
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-09 11:51:12 -0400

    sensor :(

--- comms/sensor.py
+++ comms/sensor.py
@@ -228,13 +228,14 @@ class SQMLU(SQM):
         try:
             self.addr = device_addr
             self.s = serial.Serial(self.addr, self.bauds, timeout=2)
+            self.start_connection()
         except:  # device not at that address
             print(
                 f"Device not found on {device_addr}, searching for device address ..."
             )
             self.addr = self.search()
             print(("Found address %s ... " % str(self.addr)))
-        self.start_connection()
+            self.start_connection()
         self.clear_buffer()
 
     def search(self) -> str:
