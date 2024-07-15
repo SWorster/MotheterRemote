40035a4535156e105f04ad43bac1446575d2eacb
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-15 14:53:43 -0400

    .

--- scripts/runradio.sh
+++ scripts/runradio.sh
@@ -1,4 +1,16 @@
 # * * * * * ~/MotheterRemote/scripts/runradio.sh
-if [ $(ps -ef | grep [l]ora_child) = "" ] ; then
-    /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > tmp/lora_child.log 2>&1
+# if [ $(ps -ef | grep [l]ora_child) = "" ] ; then
+#     /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > tmp/lora_child.log 2>&1
+# fi
+
+processes=$(ps -ef | grep [l]ora_child))
+if [[ $? == 1 ]]; then # grep found nothing
+    echo "Not running rpi_wifi! Running..."
+    /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > tmp/lora_child.log 2>&1 &
+elif [[ $processes ]]; then # grep returned something
+    echo "Already running rpi_wifi"
+elif [[ $? != 0 ]]; then # error
+    echo "Command failed (not grep)."
+else # something else went wrong
+    echo "Command failed."
 fi
--- scripts/runrpi.sh
+++ scripts/runrpi.sh
@@ -1,8 +1,4 @@
 # * * * * * ~/MotheterRemote/scripts/runrpi.sh
-# if [ $(ps -ef | grep [r]pi_wifi) = "" ] ; then
-#     /usr/bin/python3 ~/MotheterRemote/comms/rpi_wifi.py > /tmp/rpi_wifi.log 2>&1
-# fi
-
 processes=$(ps -ef | grep [r]pi_wifi)
 if [[ $? == 1 ]]; then # grep found nothing
     echo "Not running rpi_wifi! Running..."
@@ -10,17 +6,7 @@ if [[ $? == 1 ]]; then # grep found nothing
 elif [[ $processes ]]; then # grep returned something
     echo "Already running rpi_wifi"
 elif [[ $? != 0 ]]; then # error
-    echo "Command failed."
-else
+    echo "Command failed (not grep)."
+else # something else went wrong
     echo "Command failed."
 fi
-
-
-# if [[ $? != 0 ]]; then
-#     echo "Command failed."
-# elif [[ $processes ]]; then
-#     echo "Already running rpi_wifi"
-# else
-#     echo "Not running rpi_wifi! Running..."
-#     /usr/bin/python3 ~/MotheterRemote/comms/rpi_wifi.py > /tmp/rpi_wifi.log 2>&1
-# fi
