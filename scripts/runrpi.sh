59cb22fa477b8321be45ad14adb5cf41692ce78e
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-15 15:13:20 -0400

    trying nohup

--- scripts/runrpi.sh
+++ scripts/runrpi.sh
@@ -2,7 +2,7 @@
 processes=$(ps -ef | grep [r]pi_wifi)
 if [[ $? == 1 ]]; then # grep found nothing
     echo "Not running rpi_wifi! Running..."
-    /usr/bin/python3 ~/MotheterRemote/comms/rpi_wifi.py > /tmp/rpi_wifi.log 2>&1 &
+    nohup /usr/bin/python3 ~/MotheterRemote/comms/rpi_wifi.py > /tmp/rpi_wifi.log 2>&1
 elif [[ $processes ]]; then # grep returned something
     echo "Already running rpi_wifi"
 elif [[ $? != 0 ]]; then # error
