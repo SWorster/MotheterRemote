2c127f5963c77bf8cadaae7fdb2dd84b23592f25
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-15 14:56:38 -0400

    .

--- scripts/runradio.sh
+++ scripts/runradio.sh
@@ -1,12 +1,12 @@
 # * * * * * ~/MotheterRemote/scripts/runradio.sh
 # if [ $(ps -ef | grep [l]ora_child) = "" ] ; then
-#     /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > tmp/lora_child.log 2>&1
+#     /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > /tmp/lora_child.log 2>&1
 # fi
 
 processes=$(ps -ef | grep [l]ora_child)
 if [[ $? == 1 ]]; then # grep found nothing
     echo "Not running rpi_wifi! Running..."
-    /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > tmp/lora_child.log 2>&1 &
+    /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > /tmp/lora_child.log 2>&1 &
 elif [[ $processes ]]; then # grep returned something
     echo "Already running rpi_wifi"
 elif [[ $? != 0 ]]; then # error
