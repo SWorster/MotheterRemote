53fbeba85f7ccaa2d1f42e9572e99f0f2f79566d
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-15 14:28:54 -0400

    script experiment

--- scripts/runrpi.sh
+++ scripts/runrpi.sh
@@ -1,4 +1,14 @@
 # * * * * * ~/MotheterRemote/scripts/runrpi.sh
-if [ $(ps -ef | grep [r]pi_wifi) = "" ] ; then
+# if [ $(ps -ef | grep [r]pi_wifi) = "" ] ; then
+#     /usr/bin/python3 ~/MotheterRemote/comms/rpi_wifi.py > tmp/rpi_wifi.log 2>&1
+# fi
+
+processes = $(ps -ef | grep [r]pi_wifi)
+if [[ $? != 0 ]]; then
+    echo "Command failed."
+elif [[ $processes ]]; then
+    echo "Already running rpi_wifi"
+else
+    echo "Not running rpi_wifi! Running..."
     /usr/bin/python3 ~/MotheterRemote/comms/rpi_wifi.py > tmp/rpi_wifi.log 2>&1
 fi
