ae907b52792d21b7cff070ea94ce634af023fdf5
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-15 15:44:43 -0400

    .

--- scripts/runradio.sh
+++ scripts/runradio.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 processes=$(ps -ef | grep [l]ora_child)
 if [[ $? == 1 ]]; then # grep found nothing
-    echo "Not running rpi_wifi! Running..."
+    echo "Not running lora_child! Running..."
     nohup /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > /tmp/lora_child.log 2>&1 &
 elif [[ $processes ]]; then # grep returned something
-    echo "Already running rpi_wifi"
+    echo "Already running lora_child"
 elif [[ $? != 0 ]]; then # error
     echo "Command failed (not grep)."
 else # something else went wrong
