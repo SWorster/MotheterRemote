b6e37f5c5469eb04f4e5d3816c9511d61a3668a8
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-15 15:17:02 -0400

    nohup and ampersand?

--- scripts/runradio.sh
+++ scripts/runradio.sh
@@ -6,7 +6,7 @@
 processes=$(ps -ef | grep [l]ora_child)
 if [[ $? == 1 ]]; then # grep found nothing
     echo "Not running rpi_wifi! Running..."
-    nohup /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > /tmp/lora_child.log 2>&1
+    nohup /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py > /tmp/lora_child.log 2>&1 &
 elif [[ $processes ]]; then # grep returned something
     echo "Already running rpi_wifi"
 elif [[ $? != 0 ]]; then # error
