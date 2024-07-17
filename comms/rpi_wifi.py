51de453078a8cf4c335d8cb1f1ffceb1f7c6b631
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-17 11:12:54 -0400

    .

--- comms/rpi_wifi.py
+++ comms/rpi_wifi.py
@@ -96,7 +96,9 @@ class ThreadedTCPRequestHandler(socketserver.BaseRequestHandler):
         p(f"Received from {self.client_address[0]} in {cur_thread.name}: {self.data}")
         global output
         if "rsync" in self.data:
+            p("rsync found")
             if isinstance(output, sensor.SQMLU or sensor.SQMLE):
+                p("isinstance")
                 return  # don't send rsync message to sensor
         try:
             output.rpi_to_client(self.data)  # forward message to radio/sensor
