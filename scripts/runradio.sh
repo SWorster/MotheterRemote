b6baf89327ec579910ae200b85bedcb7fdcb381c
SWorster <131273379+SWorster@users.noreply.github.com>
2024-07-18 09:27:38 -0400

    shell typo

--- scripts/runradio.sh
+++ scripts/runradio.sh
@@ -4,7 +4,7 @@ processes=$(ps -ef | grep [l]ora_child)
 if [[ $? == 1 ]]; then # grep found nothing
     echo "Not running lora_child! Running..."
     nohup /usr/bin/python3 ~/MotheterRemote/comms/lora_child.py >> /tmp/lora_child 2>&1 &
-    echo "Will not output again until rpi_wifi stops or an error occurs."
+    echo "Will not output again until lora_child stops or an error occurs."
 elif [[ $processes ]]; then # grep returned something
     : # do nothing
 elif [[ $? != 0 ]]; then # error
